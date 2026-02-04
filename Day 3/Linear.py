3acfb45311d74c02b3ba8cfec5d262487ebe53ec
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-02-04 12:22:43 +0530

    feat: add initial implementation of Simple Linear Regression with data generation and evaluation metrics

---
+++ Day 3/Linear.py
@@ -0,0 +1,24 @@
+
+# !  Write python program to implement Simple Linear Regression from scratch using two 
+# ! different optimization methods—Ordinary Least Squares (Closed-form) and Gradient Descent 
+# ! (Iterative)—and evaluate their performance using five key regression metrics.  
+# ! Part 1: Data Generation  
+# ! Generate a synthetic dataset with a linear relationship and some added noise:  
+# ! x values: 100 random points between 0 and 50.  
+# ! y values: y = 3x + 7 + noise (where noise is a random Gaussian distribution).  
+# ! Part 2: Implementation (No Libraries)  
+# ! You are prohibited from using scikit-learn for the model or metrics. You may use NumPy for 
+# ! array operations and Matplotlib for plotting.  
+# ! Function 1: fit_ols(x, y) – Returns m and c using the closed-form equations. 
+# ! Function 2: fit_gd(x, y, learning_rate, epochs) – Returns m and c after n iterations.  
+# ! Function 3: evaluate_metrics(y_true, y_pred, n_features) – Returns a dictionary of the 
+# ! five metrics.  
+# ! Part 3: The Metrics Suite Calculate the following for both models: MAE, MSE, RMSE, R2, 
+# ! Adjusted R2 score.
+
+
+
+
+
+
+
