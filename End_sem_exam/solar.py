5e8375c98310e2e04d648427eccda16271d52617
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-04-20 09:36:32 +0530

    feat: add initial implementation of solar panel energy prediction and startup electricity consumption prediction

---
+++ End_sem_exam/solar.py
@@ -0,0 +1,25 @@
+
+# !A solar panel company tracks sunlight hours vs energy generated.
+
+#! Train a gradient descent based linear regression model (without using any library function).
+
+#! You need to:
+
+#! Experiment with 3 learning rates:
+#! 0.001
+#! 0.01
+#! 0.1
+#! Plot and analyze convergence behavior
+
+
+#! Dataset:
+
+
+#! | Sunlight Hours | Energy (kWh) |
+#! | -------------- | ------------ |
+#! | 2              | 1.5          |
+#! | 3              | 2.0          |
+#! | 4              | 2.8          |
+#! | 5              | 3.3          |
+#! | 6              | 4.0          |
+#! | 7              | 4.6          |
---
+++ End_sem_exam/startup.py
@@ -0,0 +1,23 @@
+
+#! A startup wants to predict the monthly electricity consumption of its office based on total working hours logged by employees.
+
+# !Write a Python program (without using any inbuilt ML functions) to construct an OLS-based simple linear regression model.
+
+#! You need to:
+
+#! Compute slope
+#! Compute intercept
+#! Visually plot the best-fit line over the dataset
+
+
+
+#! Dataset:
+
+#! | Working Hours | Electricity Consumption (kWh) |
+#! | ------------- | ----------------------------- |
+#! | 5             | 12                            |
+#! | 6             | 15                            |
+#! | 8             | 20                            |
+#! | 10            | 25                            |
+#! | 12            | 30                            |
+#! | 15            | 38                            |
